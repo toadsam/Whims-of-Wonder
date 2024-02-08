1f6fa3f80b3256cd2c2c6b9e59988b0a03098089
toadsam <75034052+toadsam@users.noreply.github.com>
2024-02-08 20:38:53 +0900

    [ADD] 플레이어 인풋액션 코드 추가 (-재훈)
    플레이어 인풋액션 코드 추가

--- Assets/Scripts/Characters/Player/PlayerInput.cs
+++ Assets/Scripts/Characters/Player/PlayerInput.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public PlayerInputActions InputActions { get; private set; }
+    public PlayerInputActions.PlayerActions PlayerActions { get; private set; }
+
+    private void Awake()
     {
-        
+        InputActions = new PlayerInputActions();
+
+        PlayerActions = InputActions.Player;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        InputActions.Enable();
     }
+    private void OnDisable()
+    {
+        InputActions.Disable();
+    }
+    // Start is called before the first frame update
+        
 }
